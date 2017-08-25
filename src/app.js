13e14207274dc8a2f6a4d537e93362183a9485d6
Filiz Altıntürk <info@filizaltinturk.com>
2017-08-25 08:17:12 +0300

    Colors

--- src/app.js
+++ src/app.js
@@ -14,17 +14,59 @@ Vue.component('contact-list', {
 				<span class="list-item__name">{{ child.Name }}</span>
 				<span class="list-item__phone">{{ child.Phone }}</span>
                 <span class="list-item__city">{{ child.City }}</span>
-                <button v-on:click="deleteItems(child.ID)">Delete</button>
-			<contact-list v-for="item in child.sub" :key="item.id" :child="item" :class="{ 'open' : child.toggle }" v-on:delete="deleteItem(item.ID)"></contact-list>
+                <button v-on:click="deleteItems(child)">Delete</button>
+			<contact-list v-for="item in child.sub" :key="item.ID" :child="item" :class="{ 'open' : child.toggle }" v-on:delete="deleteItem(item.ID)"></contact-list>
             </div>
 		</div>
     `,
     methods: {
         deleteItems: function (item) {
-          this.$emit('delete')
+			let self = this;
+
+			console.log(item.parentID);
+			console.log(item.ID);
+
+			// function removeFromTree(parent, childNameToRemove){
+			// 	parent = parent.filter((child) => {
+			// 		return child.ID !== childNameToRemove
+			// 	}).map((child) => {
+			// 		return removeFromTree(child.sub, childNameToRemove)
+			// 	});
+			// 	return parent;
+			//   }
+
+			//   let tree = removeFromTree(self.$root.$data.result, item.ID)         
+			//   console.log(tree);
+			
+			function finded(arr, id, param) {
+				return arr.filter((el) => {
+					let result = false;
+					if ((el[param] != id) && el.sub.length) {
+						return finded(el.sub, id, 'parentID')
+					} else if (el[param] === id) {
+						console.log('hello')
+						return el;
+
+					} else return false;
+					//result = ((el[param] != id) && el.sub.length) ? finded(el.sub, id, 'parentID') : el;
+					// if ((el[param] != id) && el.sub.length) return finded(el.sub, id, 'parentID');
+					// if (el[param] === id) return el;
+					// return result;
+				})
+			}
+			//console.log(finded(self.$root.$data.result, item.ID, 'ID'))
+			
+
+			//   self.$emit('delete')
+		// let index = this.$root.$data.result.indexOf(item);
+		// this.$delete(this.$root.$data.result, item.id);
+		// console.log(item.id)
+		// console.log(this.$root.$data.result)
+		//this.$root.$data.result.splice(index, 1)
         }
     },
 })
+
 let contact = new json({
 	el: '#list',
 	data: {},
@@ -35,13 +77,13 @@ let contact = new json({
 	methods: {
         deleteItem: function (id) {
 			let self = this;
-
-			self.result.map(item => { 
-				 if (item.ID === id) {
-					 let index = self.result.indexOf(item);
-					 self.result.splice(index, 1)
-				 };
-			})
+			console.log(id);
+			// self.result.map(item => { 
+			// 	 if (item.ID === id) {
+			// 		 let index = self.result.indexOf(item);
+			// 		 self.result.splice(index, 1)
+			// 	 };
+			// })
 		},
 		contactList: function() {
 			let self = this;
@@ -108,6 +150,52 @@ let contact = new json({
         //     }
         
 		
+		},
+		getList: function () {
+			let self = this;
+			
+			let parent = self.arr.filter(item => {
+				if (typeof item.parentID === 'undefined') setRead(item.ID);
+				return ((typeof item.parentID === 'undefined') || (item.parentID === 0)); 
+			});
+
+			function readed(data) {
+				let result = data.filter(item => {
+					return !item.readed; 
+				});
+				return result;
+			}
+			
+			function setRead(id) {
+				let result = self.arr.map(item => { 
+					if (item.ID === id) item.readed = true;
+					return item;
+				})
+				self.arr = result;
+				return result;
+			}
+
+			function findChild(arr, id) {
+				let result = arr.filter(item => {
+					if (item.parentID === id) setRead(item.ID);
+					return (item.parentID === id);
+				});
+				return result; 
+			}
+			
+			function list(arr, data) {
+				data.filter(item => { 
+					let result = findChild(readed(arr), item.ID); 
+					//item.sub = result; 
+					self.$set(item, 'sub', result);
+					self.$set(item, 'toggle', false);
+
+					if (item.sub.length > 0) list(readed(arr), item.sub);
+				})
+				return data;
+            }
+            
+			return list(self.arr, parent);
 		}
 		
 	},
